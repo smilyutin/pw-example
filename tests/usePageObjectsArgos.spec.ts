cc4132d3da86eb281fb2e8bca0186bfc61c95b5e
smilyutin <you@example.comsmilyutin1@gmail.com>
2025-08-13 10:27:49 -0700

    Argos

---
+++ tests/usePageObjectsArgos.spec.ts
@@ -0,0 +1,24 @@
+// tests/usePageObjects.spec.ts
+import { test, expect } from '@playwright/test';                // Playwright test runner APIs
+import { PageManager } from '../page-objects/pageManager';         // Centralized page manager
+import { faker} from '@faker-js/faker'
+import { argosScreenshot } from "@argos-ci/playwright";
+
+// Start the Angular app before each test
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:4200/');                      // Navigate to the app root
+});
+
+test('testing with agros ci', async ({ page }) => {
+  test.slow();                                                   // Mark this test as slow for extra timeout
+  const pm = new PageManager(page);                              // Instantiate manager with the Playwright page
+
+  // Use the navigation page under the manager to visit each section
+  await pm.navigateTo().formLayoutsPage();  
+  await argosScreenshot(page, "forms layout page");                     // Click "Form Layouts"
+  await pm.navigateTo().datepickerPage(); 
+  await argosScreenshot(page, "date picker page");                       // Click "Datepicker"
+  // await pm.navigateTo().smartTablePage();                        // Click "Smart Table"
+  // await pm.navigateTo().toastrPage();                           // Click "Toastr"
+  // await pm.navigateTo().tooltipPage();                          // Click "Tooltip"
+});
